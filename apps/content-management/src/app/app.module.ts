5bce7130564d1f829f15108c0840783509f13049
memotesgoldenstein <83883768+memotesgoldenstein@users.noreply.github.com>
2021-11-10 01:47:40 +0300

    angular material imports organised

--- apps/content-management/src/app/app.module.ts
+++ apps/content-management/src/app/app.module.ts
@@ -1,19 +1,23 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
-import { MaterialModule, materialRoutes } from '@aura-ng-nj/material';
-import { CoreDataModule, coreDataRoutes } from '@aura-ng-nj/core-data';
-import { CoreStateModule, coreStateRoutes } from '@aura-ng-nj/core-state';
-import { QuestionsComponent } from './questions/questions.component';
-import { QuestionsListComponent } from './questions/questions-list/questions-list.component';
-import { QuestionDetailsComponent } from './questions/question-details/question-details.component';
-import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreDataModule } from '@aura-ng-nj/core-data';
+import { CoreStateModule } from '@aura-ng-nj/core-state';
+import { MaterialModule } from '@aura-ng-nj/material';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { QuestionDetailsComponent } from './questions/question-details/question-details.component';
+import { QuestionsListComponent } from './questions/questions-list/questions-list.component';
+import { QuestionsComponent } from './questions/questions.component';
 
 @NgModule({
-  declarations: [AppComponent, QuestionsComponent, QuestionsListComponent, QuestionDetailsComponent],
+  declarations: [
+    AppComponent,
+    QuestionsComponent,
+    QuestionsListComponent,
+    QuestionDetailsComponent,
+  ],
   imports: [
     BrowserModule,
     HttpClientModule,
@@ -28,7 +32,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   exports: [
     QuestionsComponent,
     QuestionsListComponent,
-    QuestionDetailsComponent
+    QuestionDetailsComponent,
   ],
 })
 export class AppModule {}
